ec1c5d400b1fb68bb5b12df5d54e8346011ee9d3
Ivan Teteruk <iteterk@gmail.com>
2024-01-31 10:16:32 +0300

    fix some bugs

--- navMenu.js
+++ navMenu.js
@@ -1,8 +1,59 @@
 const burgerBtn = document.getElementById('burger-btn');
 const navMenu = document.getElementById('nav-Menu');
-const icon = document.getElementById('burger-btn');
+const subNav = document.querySelector(`.subnav`)
 
 burgerBtn.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-    burgerBtn.classList.toggle('open');
+  navMenu.classList.toggle('active');
+  burgerBtn.classList.toggle('open');
 })
+
+const navIds = [
+  "aboutCompany",
+  "production",
+  "services",
+  "services",
+  "services"
+]
+const navIdsSub = [
+  "kadr",
+]
+
+navIds.forEach(id => {
+  const subNavContent = document.getElementById(`nav-${id}`);
+  
+  document.getElementById(id).addEventListener('click', () => {
+    navIds.forEach(ids => {
+      if (ids != id) {
+        document.getElementById(`nav-${ids}`).classList.remove('open');
+      }
+    })
+
+    if (subNavContent.classList.contains('open')) {
+      subNavContent.classList.remove('open');
+    } else {
+      subNavContent.classList.add('open');
+    }
+  })
+})
+
+function hideSubNav(id) {
+  let item = document.getElementById(id);
+  if(item.classList.contains('open')) {
+    item.classList.remove('open');
+  } else {
+    item.classList.add('open');
+  }
+}
+
+navIdsSub.forEach(id => {
+  document.getElementById(id).addEventListener('click', () => {
+    document.querySelector(`.subnav__container_${id}`).classList.add('openclick');
+  })
+  document.getElementById(id).addEventListener('mouseover', () => {
+    document.querySelector(`.subnav__container_${id}`).classList.add('open');
+  })
+  document.getElementById(id).addEventListener('mouseout', () => {
+    document.querySelector(`.subnav__container_${id}`).classList.remove('open');
+  })
+})
+
